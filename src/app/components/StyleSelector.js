b6c92d6b987401cc41469c05d8eb316791377ecb
abdu1o <a.kapara2006@gmail.com>
2024-10-23 20:48:36 +0300

    +players selector

--- src/app/components/StyleSelector.js
+++ src/app/components/StyleSelector.js
@@ -58,7 +58,27 @@ const StyleSelector = ({ handleAddClick }) => {
                 <option value="zeus">Zeus</option>
             </select>
 
-            <input type="text" id="killerField" className="input" placeholder="Killer name" />
+            <div className="player-container">
+                <input type="text" id="killer-name" className="input" placeholder="Killer name" />
+                <select id="killer-side" className="sideSelector">
+                    <option value="T">T</option>
+                    <option value="CT">CT</option>
+                </select>
+            </div>
+            <div className="player-container">
+                <input type="text" id="assist-name" className="input" placeholder="Assist name" />
+                <select id="assist-side" className="sideSelector">
+                    <option value="T">T</option>
+                    <option value="CT">CT</option>
+                </select>
+            </div>
+            <div className="player-container">
+                <input type="text" id="victim-name" className="input" placeholder="Victim name" />
+                <select id="victim-side" className="sideSelector">
+                    <option value="T">T</option>
+                    <option value="CT">CT</option>
+                </select>
+            </div>
             <button
                 id="addKillfeed"
                 className="button"
--- src/app/page.js
+++ src/app/page.js
@@ -9,32 +9,50 @@ export default function Home() {
   const handleAddClick = () => {
     const killfeedStyleSelect = document.getElementById('killfeedStyle-select');
     const weapon = document.getElementById('weapon-killer');
+    const killerName = document.getElementById('killer-name');
+    const killerSide = document.getElementById('killer-side');
+
+    const assistName = document.getElementById('assist-name');
+    const assistSide = document.getElementById('assist-side');
+
+    const victimName = document.getElementById('victim-name');
+    const victimSide = document.getElementById('victim-side');
+
     const selectedStyle = killfeedStyleSelect.value;
     const selectedWeapon = weapon.value;
+
+    const selectedKillerName = killerName.value;
+    const selectedKillerSide = killerSide.value;
+
+    const selectedAssistName = assistName.value;
+    const selectedAssistSide = assistSide.value;
+
+    const selectedVictimName = victimName.value;
+    const selectedVictimSide = victimSide.value;
 
     const killFeedData = {
       game: "CS2",
       colors: selectedStyle,
       events: [
         {
-          killerName: "player1",
-          side: "CT",
+          killerName: selectedKillerName,
+          side: selectedKillerSide,
           weapon: selectedWeapon,
-          noscope: false,
-          wallbang: false,
-          smoke: false,
-          headshot: false,
-          blindfolded: false,
-          airkill: false,
-          flashassist: false,
+          noscope: true,
+          wallbang: true,
+          smoke: true,
+          headshot: true,
+          blindfolded: true,
+          airkill: true,
+          flashassist: true,
           assist: {
-            name: "",
-            side: "CT"
+            name: selectedAssistName,
+            side: selectedAssistSide
           }
         },
         {
-          victimName: "player1",
-          side: "T",
+          victimName: selectedVictimName,
+          side: selectedVictimSide,
         }
       ]
     };

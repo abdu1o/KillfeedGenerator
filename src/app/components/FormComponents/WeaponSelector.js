43cfb0c50083fbcd50b5314175ec4d97afada22b
abdu1o <a.kapara2006@gmail.com>
2024-10-24 17:03:56 +0300

    weapon selector fix

--- src/app/components/FormComponents/WeaponSelector.js
+++ src/app/components/FormComponents/WeaponSelector.js
@@ -1,54 +1,62 @@
 import React from "react";
 import "../../styles/styleContainer.css";
 
+const weapons = [
+    { value: "ak47", label: "AK-47" },
+    { value: "aug", label: "AUG" },
+    { value: "awp", label: "AWP" },
+    { value: "bayonet", label: "Bayonet" },
+    { value: "bizon", label: "PP-Bizon" },
+    { value: "burned", label: "Burned" },
+    { value: "cz75", label: "CZ75-Auto" },
+    { value: "deagle", label: "Desert Eagle" },
+    { value: "dualBerettas", label: "Dual Berettas" },
+    { value: "famas", label: "FAMAS" },
+    { value: "fiveSeven", label: "Five-Seven" },
+    { value: "flip", label: "Flip" },
+    { value: "galil", label: "Galil AR" },
+    { value: "g3sg1", label: "G3SG1" },
+    { value: "glock", label: "Glock" },
+    { value: "gut", label: "Gut" },
+    { value: "heGranade", label: "HE Granade" },
+    { value: "karambit", label: "Karambit" },
+    { value: "knifeCT", label: "Knife CT" },
+    { value: "knifeT", label: "Knife T" },
+    { value: "m249", label: "M249" },
+    { value: "m4a1s", label: "M4A1-S" },
+    { value: "m4a4", label: "M4A4" },
+    { value: "mac10", label: "Mac-10" },
+    { value: "mag7", label: "MAG7" },
+    { value: "m9Bayonet", label: "M9 Bayonet" },
+    { value: "mp5", label: "MP5" },
+    { value: "mp7", label: "MP7" },
+    { value: "mp9", label: "MP9" },
+    { value: "negev", label: "Negev" },
+    { value: "nova", label: "Nova" },
+    { value: "p2000", label: "p2000" },
+    { value: "p250", label: "P250" },
+    { value: "p90", label: "P90" },
+    { value: "revolver", label: "R8 Revolver" },
+    { value: "scar20", label: "SCAR-20" },
+    { value: "sawedOff", label: "Sawed-Off" },
+    { value: "sg553", label: "SG 553" },
+    { value: "ssg08", label: "SSG 08" },
+    { value: "suicide", label: "Suicide" },
+    { value: "tec9", label: "Tec-9" },
+    { value: "ump45", label: "UMP-45" },
+    { value: "usp", label: "USP-S" },
+    { value: "xm1014", label: "XM1014" },
+    { value: "zeus", label: "Zeus" }
+];
+
 const WeaponSelector = () => {
     return (
         <select id="weapon-killer" className="selector">
-            <option value="ak47">AK-47</option>
-            <option value="aug">AUG</option>
-            <option value="awp">AWP</option>
-            <option value="bayonet">Bayonet</option>
-            <option value="bizon">PP-Bizon</option>
-            <option value="burned">Burned</option>
-            <option value="cz75">CZ75-Auto</option>
-            <option value="deagle">Desert Eagle</option>
-            <option value="dualBerettas">Dual Berettas</option>
-            <option value="famas">FAMAS</option>
-            <option value="fiveSeven">Five-Seven</option>
-            <option value="flip">Flip</option>
-            <option value="galil">Galil AR</option>
-            <option value="g3sg1">G3SG1</option>
-            <option value="glock">Glock</option>
-            <option value="gut">Gut</option>
-            <option value="heGranade">HE Granade</option>
-            <option value="karambit">Karambit</option>
-            <option value="knifeCT">Knife CT</option>
-            <option value="knifeT">Knife T</option>
-            <option value="m249">M249</option>
-            <option value="m4a1s">M4A1-S</option>
-            <option value="m4a4">M4A4</option>
-            <option value="mac10">Mac-10</option>
-            <option value="mag7">MAG7</option>
-            <option value="m9Bayonet">M9 Bayonet</option>
-            <option value="mp5">MP5</option>
-            <option value="mp7">MP7</option>
-            <option value="mp9">MP9</option>
-            <option value="negev">Negev</option>
-            <option value="nova">Nova</option>
-            <option value="p2000">p2000</option>
-            <option value="p250">P250</option>
-            <option value="p90">P90</option>
-            <option value="revolver">R8 Revolver</option>
-            <option value="scar20">SCAR-20</option>
-            <option value="sawedOff">Sawed-Off</option>
-            <option value="sg553">SG 553</option>
-            <option value="ssg08">SSG 08</option>
-            <option value="suicide">Suicide</option>
-            <option value="tec9">Tec-9</option>
-            <option value="ump45">UMP-45</option>
-            <option value="usp">USP-S</option>
-            <option value="xm1014">XM1014</option>
-            <option value="zeus">Zeus</option>
+            {weapons.map(weapon => (
+                <option key={weapon.value} value={weapon.value}>
+                    {weapon.label}
+                </option>
+            ))}
         </select>
     );
 };
